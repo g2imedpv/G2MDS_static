3aafda03aba5532ad1730a3be9fee71e9b603f78
wenpei <wenpei.wang@g2-biopharma-services.com>
2019-05-22 12:36:40 -0400

    updated

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET Home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'G2-MDS Home' });
 });
 
-/* GET service page */
+/* GET Service page */
 router.get('/service', function(req,res,next) {
   res.render('service', { title: 'Service'});
 });
 
-/* GET about page */
+/* GET Contact page */
+router.get('/contact', function(req,res,next) {
+  res.render('contact', { title: 'Contact Us'});
+});
+
+/* GET About page */
 router.get('/about', function(req,res,next) {
-  res.render('about', { title: 'About'});
+  res.render('about', { title: 'About Us'});
 });
 
 module.exports = router;
